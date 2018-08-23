73a5ee3b064768269bff0601aa37a0dffa0719cd
dong <>
2018-08-23 22:08:04 +0800

    Jump Game II

--- dong/cpp/p_45.cpp
+++ dong/cpp/p_45.cpp
@@ -12,7 +12,7 @@ int jump(vector<int> &nums)
     int sz = nums.size();
     if (sz == 1)
         return 0;
-    int pos = 0, sat = 0, m_dis = 0, step = 0;
+    int pos = 0, sat = 0, m_dis = 0, step = 0, i = 0;
     while (true)
     {
         m_dis = 0;
@@ -21,7 +21,7 @@ int jump(vector<int> &nums)
         {
             return step + 1;
         }
-        for (int i = sat + 1; i <= pos; i++)
+        for (; i <= pos; i++)
         {
             if (nums[i] + i > m_dis)
             {
@@ -31,4 +31,20 @@ int jump(vector<int> &nums)
         }
         step++;
     }
+}
+
+int jump_concise(vector<int> nums)
+{
+    int sz = nums.size(), pos = 0, farthest = 0, step = 0;
+    for (int i = 0; i < sz - 1; i++)
+    {
+        farthest = max(i + nums[i], farthest);
+
+        if (i == pos)
+        {
+            pos = farthest;
+            step++;
+        }
+    }
+    return step;
 }
