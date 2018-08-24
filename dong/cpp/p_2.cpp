91196a6b70f7ad8e600f7441d7536ee4d1a6697d
dong <>
2018-08-25 01:23:24 +0800

    Add Two Numbers

--- dong/cpp/p_2.cpp
+++ dong/cpp/p_2.cpp
@@ -1,5 +1,8 @@
-// Definition for singly-linked list.
 #include <cstdio>
+
+/*
+    Add Two Numbers
+*/
 
 struct ListNode
 {
@@ -8,62 +11,36 @@ struct ListNode
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-
-class Solution
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
-  public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    int c = 0;
+    ListNode *n1 = l1, *n2 = l2, *pre = NULL;
+    while (n1 != NULL && n2 != NULL)
     {
-        int c = 0;
-        ListNode *n1 = l1, *n2 = l2, *pre = NULL;
-        while (n1 != NULL && n2 != NULL)
-        {
-            int tmp = n1->val + n2->val + c;
-            n1->val = tmp % 10;
-            c = tmp / 10;
-            pre = n1;
-            n1 = n1->next;
-            n2 = n2->next;
-        }
-        if (n2 != NULL)
-        {
-            pre->next = n2;
-            n1 = n2;
-        }
-        while (n1 != NULL)
-        {
-            int tmp = n1->val + c;
-            n1->val = tmp % 10;
-            c = tmp / 10;
-            pre = n1;
-            n1 = n1->next;
-        }
-        if (c != 0)
-        {
-            ListNode *node = new ListNode(c);
-            pre->next = node;
-        }
-        return l1;
+        int tmp = n1->val + n2->val + c;
+        n1->val = tmp % 10;
+        c = tmp / 10;
+        pre = n1;
+        n1 = n1->next;
+        n2 = n2->next;
     }
-};
-
-
-int main(){
-    Solution s;
-    ListNode *l1 = new ListNode(2);
-    ListNode *l2 = new ListNode(5);
-
-    l1->next = new ListNode(4);
-    l1->next->next = new ListNode(3);
-    l2->next = new ListNode(6);
-    l2->next->next = new ListNode(4);
-
-    ListNode *ans = s.addTwoNumbers(l1, l2);
-    ListNode *p = ans;
-    while(p!=NULL){
-        printf("%d", p->val);
-        if(p->next!=NULL) printf("->");
-        p = p->next;
+    if (n2 != NULL)
+    {
+        pre->next = n2;
+        n1 = n2;
     }
-    printf("\n");
+    while (n1 != NULL)
+    {
+        int tmp = n1->val + c;
+        n1->val = tmp % 10;
+        c = tmp / 10;
+        pre = n1;
+        n1 = n1->next;
+    }
+    if (c != 0)
+    {
+        ListNode *node = new ListNode(c);
+        pre->next = node;
+    }
+    return l1;
 }
