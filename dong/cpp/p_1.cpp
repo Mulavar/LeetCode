e98765e437ae9e35c175c0cb422903a9831f02ca
dong <>
2018-08-25 01:24:21 +0800

    Two Sum

--- dong/cpp/p_1.cpp
+++ dong/cpp/p_1.cpp
@@ -4,37 +4,25 @@
 
 using namespace std;
 
-class Solution
+/*
+	Two Sum
+*/
+
+vector<int> twoSum(vector<int> &nums, int target)
 {
-  public:
-    vector<int> twoSum(vector<int> &nums, int target)
+    map<int, int> mp;
+    int l = 0;
+    vector<int> res;
+    while (l < nums.size())
     {
-        map<int, int> mp;
-        int l = 0;
-        vector<int> res;
-        while (l < nums.size())
+        if (mp.find(target - nums[l]) != mp.end())
         {
-            if (mp.find(target - nums[l]) != mp.end())
-            {
-                res.push_back(mp[target - nums[l]]);
-                res.push_back(l);
-                return res;
-            }
-            mp[nums[l]] = l;
-            l++;
+            res.push_back(mp[target - nums[l]]);
+            res.push_back(l);
+            return res;
         }
-        return res;
+        mp[nums[l]] = l;
+        l++;
     }
-};
-
-int main()
-{
-    vector<int> vec;
-    Solution s;
-    vec.push_back(3);
-    vec.push_back(5);
-    vec.push_back(4);
-
-    vector<int> ans = s.twoSum(vec, 7);
-    printf("%d %d\n", ans[0], ans[1]);
+    return res;
 }
