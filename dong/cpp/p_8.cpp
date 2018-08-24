3500ecb377a0c85ee7c4aaeafde73bed04dd6893
dong <>
2018-08-25 01:22:01 +0800

    String to Integer (atoi)

--- dong/cpp/p_8.cpp
+++ dong/cpp/p_8.cpp
@@ -8,37 +8,40 @@ using namespace std;
     String to Integer (atoi)
 */
 
-
-class Solution {
-public:
-    int myAtoi(string str) {
-        int len = str.length(), start = 0, constant = 1;
-        long ans = 0;
-        for(int i=0;i<len;i++){
-            if(str[i]==' ') continue;
-            if(str[i]>'9'||str[i]<'0'&&str[i]!='+'&&str[i]!='-') return 0;
-            else{
-                start = i;
-                break;
-            }
+int myAtoi(string str)
+{
+    int len = str.length(), start = 0, constant = 1;
+    long ans = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == ' ')
+            continue;
+        if (str[i] > '9' || str[i] < '0' && str[i] != '+' && str[i] != '-')
+            return 0;
+        else
+        {
+            start = i;
+            break;
         }
-        // int start = str.find_first_not_of(' ');
-        if(str[start]=='+')
-            start++;
-        else if(str[start]=='-'){
-            constant=-1;
-            start++;
-        }
-        
-        while(start<len&&str[start]<='9'&&str[start]>='0'){
-            ans = ans * 10 + str[start] - '0';
-            start++;
-            if(constant*ans>INT_MAX) return INT_MAX;
-            else if(constant*ans<INT_MIN) return INT_MIN;
-        }
-        
-        
-        return ans*constant;
-        
     }
-};
+    // int start = str.find_first_not_of(' ');
+    if (str[start] == '+')
+        start++;
+    else if (str[start] == '-')
+    {
+        constant = -1;
+        start++;
+    }
+
+    while (start < len && str[start] <= '9' && str[start] >= '0')
+    {
+        ans = ans * 10 + str[start] - '0';
+        start++;
+        if (constant * ans > INT_MAX)
+            return INT_MAX;
+        else if (constant * ans < INT_MIN)
+            return INT_MIN;
+    }
+
+    return ans * constant;
+}
