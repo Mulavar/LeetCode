e2f6b63f1a1cfb132870fc9b35a07a90503f6179
dongjianhui <dongjianhui@hyperchain.cn>
2019-08-10 19:14:05 +0800

    add p1 solution: Two Sum

---
+++ dong/java/P1TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class P1TwoSum {
+    class Solution {
+        // Solution1
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> maps = new HashMap<Integer, Integer>();
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.length; i++) {
+                if (maps.containsKey(target - nums[i])) {
+                    result[0] = i < maps.get(target - nums[i]) ? i : maps.get(target - nums[i]);
+                    result[1] = i > maps.get(target - nums[i]) ? i : maps.get(target - nums[i]);
+                    return result;
+                }
+                maps.put(nums[i], i);
+            }
+            return result;
+        }
+    }
+}
