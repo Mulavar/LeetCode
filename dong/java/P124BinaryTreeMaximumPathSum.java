2915f0a55bc42662a76835d967c20a4075317a26
Jianhui Dong <dongjianhui@hyperchain.cn>
2020-02-13 23:57:27 +0800

    feat(java): add p124 java solution

---
+++ dong/java/P124BinaryTreeMaximumPathSum.java
@@ -0,0 +1,41 @@
+/**
+ * Given a non-empty binary tree, find the maximum path sum.
+ * <p>
+ * For this problem, a path is defined as any sequence of nodes
+ * from some starting node to any node in the tree along the parent-child connections.
+ * The path must contain at least one node and does not need to go through the root.
+ *
+ * @author Lam
+ * @ClassName P124BinaryTreeMaximumPathSum
+ * @date 2020/2/13
+ */
+public class P124BinaryTreeMaximumPathSum {
+    class Solution {
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+
+            TreeNode(int x) {
+                val = x;
+            }
+
+        }
+
+        public int maxPathSum(TreeNode root) {
+            int[] result = new int[]{Integer.MIN_VALUE};
+            postOrder(root, result);
+            return result[0];
+        }
+
+        private int postOrder(TreeNode root, int[] result) {
+            if (root == null) {
+                return 0;
+            }
+            int left = Math.max(postOrder(root.left, result), 0);
+            int right = Math.max(postOrder(root.right, result), 0);
+            result[0] = Math.max(result[0], left + right + root.val);
+            return root.val + Math.max(left, right);
+        }
+    }
+}
